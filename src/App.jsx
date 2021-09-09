61c0df24f3e1ba557361a2ca88e6dea054299a03
15868707168@163.com <15868707168@163.com>
2021-09-09 14:03:06 +0800

    更新github demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
+import './App.css'
 import List from './components/List'
 import Search from './components/Search'
 
 export default class App extends Component {
+
+
+    //更新App的state
+    updateAppState=(stateObj) => {
+        this.setState(stateObj);
+    }
+
     render() {
         return (
-            <div>
-                <Search />
-                <List />
+           <div className="container">
+               <Search updateAppState={this.updateAppState}/>
+               <List />
             </div>
         )
     }
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,53 +1,40 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import axios from 'axios'
 
 export default class Search extends Component {
+
     static propTypes = {
-        prop: PropTypes
+        updateAppState: PropTypes.func
     }
+
+    search=() => {
+        //获取用户的输入(连续解构赋值+重命名)
+        const {keyWordElement:{value:keyWord}}=this;
+        console.log(this.keyWordElement.value);
+        console.log(keyWord);
+        //发送请求前通知App更新状态
+        this.props.updateAppState({isFirst:false,isLoading:true});
+        //发送网络请求
+        axios.get(`/api1/search/users?q=${keyWord}`).then(
+            response=>{
+                //请求成功后通知App更新状态
+                console.log(response.data)
+                this.props.updateAppState({isLoading:false,users:response.data})
+            }
+        )
+    }
+
 
     render() {
         return (
-            <div className="container">
-                <section className="jumbotron">
-                <h3 className="jumbotron-heading">Search Github Users</h3>
+            <section className="jumbotron">
+                <h3 className="jumbotron-heading">搜索Github用户</h3>
                 <div>
-                    <input type="text" placeholder="enter the name you search"/>&nbsp;<button>Search</button>
+                    <input ref={c=>this.keyWordElement=c} type="text" placeholder="输入关键词点击搜索"/>&nbsp;
+                    <button onClick={this.search}>Search</button>
                 </div>
-                </section>
-                <div className="row">
-                    <div className="card">
-                        <a href="https://github.com/reactjs" target='_blank' rel='noreferrer'>
-                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" alt='' style={{width:'100px'}}/>
-                        </a>
-                        <p className="card-text">reactjs</p>
-                    </div>
-                    <div className="card">
-                        <a href="https://github.com/reactjs" target="_blank" rel='noreferrer'>
-                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" alt='' style={{width:'100px'}}/>
-                        </a>
-                        <p className="card-text">reactjs</p>
-                    </div>
-                    <div className="card">
-                        <a href="https://github.com/reactjs" target="_blank" rel='noreferrer'>
-                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" alt='' style={{width:'100px'}}/>
-                        </a>
-                        <p className="card-text">reactjs</p>
-                    </div>
-                    <div className="card">
-                        <a href="https://github.com/reactjs" target="_blank" rel='noreferrer'>
-                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" alt='' style={{width:'100px'}}/>
-                        </a>
-                        <p className="card-text">reactjs</p>
-                    </div>
-                    <div className="card">
-                        <a href="https://github.com/reactjs" target="_blank" rel='noreferrer'>
-                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width:'100px'}}/>
-                        </a>
-                        <p className="card-text">reactjs</p>
-                    </div>
-                  </div>
-            </div>
+            </section>
         )
     }
 }
