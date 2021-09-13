93807ffbbe3b5c2883749fca41576bf15a18fff0
15868707168@163.com <15868707168@163.com>
2021-09-13 18:08:06 +0800

    传参

--- src/pages/Home/Message/index.jsx
+++ src/pages/Home/Message/index.jsx
@@ -1,20 +1,36 @@
 import React, { Component } from 'react'
+import {Link,Route} from 'react-router-dom'
+import Detail from './Detail'
 
 export default class index extends Component {
+	state={
+		messageArr:[
+			{id:'01',title:'消息1'},
+			{id:'02',title:'消息2'},
+			{id:'03',title:'消息3'}
+		]
+	}
+
     render() {
+		const {messageArr}=this.state
         return (
             <div>
                	<ul>
-					<li>
-						<a href="/message1">message001</a>&nbsp;&nbsp;
-					</li>
-					<li>
-						<a href="/message2">message002</a>&nbsp;&nbsp;
-					</li>
-					<li>
-						<a href="/message/3">message003</a>&nbsp;&nbsp;
-					</li>
+				{
+					messageArr.map(
+						(msgObj) => {
+							return(
+								<li key={msgObj.id}>
+									{/* 向路由组件传递params参数 */}
+									<Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link>
+								</li>
+							)
+						}
+					)
+				}
 				</ul>
+
+				<Route to="/home/message/detail/:id/:title" component={Detail}/>
             </div>
         )
     }
