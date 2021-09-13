efa1bbac2c20eccaf0a67807fdf41b15fd1fa9fb
15868707168@163.com <15868707168@163.com>
2021-09-13 11:12:11 +0800

    NavLink的简单封装

---
+++ 08_src_路由的基本使用 copy/App.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+//引入react-router-dom
+import {Link,Route} from 'react-router-dom'    
+
+//引入组件
+import About from './components/About'
+
+import Home from './components/Home'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div id="root">
+                <div>
+                    <div className="row">
+                        <div className="col-xs-offset-2 col-xs-8">
+                            <div className="page-header"><h2>React Router Demo</h2></div>
+                        </div>
+                        </div>
+                        <div className="row">
+                        <div className="col-xs-2 col-xs-offset-2">
+                            <div className="list-group">
+                            {/*
+                             原生Html中，靠<a>跳转不同的页面
+                             <a className="list-group-item active" href="./about.html">About</a>
+                             <a className="list-group-item" href="./home.html">Home</a> */}
+                                {/* 在React中靠路由链接是实现切换组件 -- 编写路由链接 */}
+                                <Link to="/about" className="list-group-item">About</Link>
+                                <Link to="/home" className="list-group-item">Home</Link>
+                            </div>
+                        </div>
+                        <div className="col-xs-6">
+                            <div className="panel">
+                                <div className="panel-body">
+                                    {/* <Switch>
+                                        <Route path='/about'>
+                                            <About />
+                                        </Route>
+                                        <Route path='/home'>
+                                            <Home />
+                                        </Route>
+                                    </Switch> */}
+                                    {/* 注册路由 */}
+                                    <Route path='/home' component={Home}/>
+                                    <Route path='/about' component={About}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ 10_src_封装NavLink/components/MyNavLink/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import {NavLink} from 'react-router-dom'
+
+export default class MyNavLink extends Component {
+    render() {
+        //console.log(this.props);
+        return (
+            <div>
+                <NavLink activeClassName='atguigu' className='list-group-item' {...this.props}></NavLink>
+            </div>
+        )
+    }
+}
